d7a5c1f78508d065af5032cd0815859f04c10164
antoaneta <antoanetapstoyanova@gmail.com>
2024-02-26 17:05:00 +0100

    csv

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import plotly.express as px
 
 st.header('Impact Product Positioning Sales', divider="grey")
 
@@ -10,16 +11,18 @@ filtered_data = data[(data['Product Category']=='Clothing')&
                      (data['Product Position'])]
 
 
-st.write(filtered_data)
 
-# Grouped bar chart using Altair
+
+
+
 chart = alt.Chart(filtered_data).mark_bar().encode(
-    x='Product Position',
-    y='mean(Price)',
-    color='Seasonal',
-    column='Seasonal'
-).properties(
-    width=200
-)
+    x=alt.X('Product Position', title=''),
+    xOffset='Seasonal',
+    y=alt.Y('mean(Price)'),
+    color=alt.Color('Seasonal').scale(scheme='pastel1'))
+st.altair_chart(chart, use_container_width=True, theme=None)
 
-st.write(chart)
+
+
+
+
